3781645d038db8c72b0e8e9c39b144868c18e2b1
Utopi-a <bridgestone.amatsu0228@gmail.com>
2024-06-23 01:00:59 +0900

    ui: リセットボタンが都道府県取得までは表示されないようにした。データフェッチ中にその文言を表示するようにした。

--- src/features/components/PopulationTrendChart/PopulationTrendChart.tsx
+++ src/features/components/PopulationTrendChart/PopulationTrendChart.tsx
@@ -16,59 +16,70 @@ import { formatData, generateColor, getPrefName } from "./utils";
 type PopulationTrendChartProps = {
   prefList: PrefApiResponse | undefined;
   populationData: PopulationProcedureOutput | undefined;
+  isFetching: boolean;
 };
 
-export const PopulationTrendChart = ({ prefList, populationData }: PopulationTrendChartProps) => {
+export const PopulationTrendChart = ({
+  prefList,
+  populationData,
+  isFetching,
+}: PopulationTrendChartProps) => {
   const [selectedPopulationLabel, setSelectedPopulationLabel] = useState("総人口");
   const { formattedData, keys } = formatData(populationData, selectedPopulationLabel);
 
   return (
     <section>
-      {formattedData && formattedData.length > 0 && (
+      {((formattedData && formattedData.length > 0) || isFetching) && (
         <div className="card">
-          <h2>都道府県別人口推移</h2>
-          <PopulationSwitchRadio
-            selectedPopulationLabel={selectedPopulationLabel}
-            setSelectedPopulationLabel={setSelectedPopulationLabel}
-          />
-          <div className="chart">
-            <ResponsiveContainer>
-              <LineChart
-                data={formattedData}
-                margin={{
-                  top: 30,
-                  right: 40,
-                  left: 40,
-                  bottom: 20,
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis
-                  dataKey="year"
-                  label={{ value: "年度（西暦）", position: "insideBottomRight", offset: -10 }}
-                />
-                <YAxis label={{ value: "人口数", position: "top", offset: 10 }} />
-                <Tooltip />
-                <Legend
-                  layout="horizontal"
-                  verticalAlign="top"
-                  align="right"
-                  wrapperStyle={{
-                    top: 10,
-                  }}
-                />
-                {keys.map((key, index) => (
-                  <Line
-                    type="monotone"
-                    dataKey={key}
-                    name={getPrefName(prefList, key) ?? key}
-                    key={key}
-                    stroke={generateColor(index, keys.length)}
-                  />
-                ))}
-              </LineChart>
-            </ResponsiveContainer>
-          </div>
+          {!isFetching ? (
+            <>
+              <h2>都道府県別人口推移</h2>
+              <PopulationSwitchRadio
+                selectedPopulationLabel={selectedPopulationLabel}
+                setSelectedPopulationLabel={setSelectedPopulationLabel}
+              />
+              <div className="chart">
+                <ResponsiveContainer>
+                  <LineChart
+                    data={formattedData}
+                    margin={{
+                      top: 30,
+                      right: 40,
+                      left: 40,
+                      bottom: 20,
+                    }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis
+                      dataKey="year"
+                      label={{ value: "年度（西暦）", position: "insideBottomRight", offset: -10 }}
+                    />
+                    <YAxis label={{ value: "人口数", position: "top", offset: 10 }} />
+                    <Tooltip />
+                    <Legend
+                      layout="horizontal"
+                      verticalAlign="top"
+                      align="right"
+                      wrapperStyle={{
+                        top: 10,
+                      }}
+                    />
+                    {keys.map((key, index) => (
+                      <Line
+                        type="monotone"
+                        dataKey={key}
+                        name={getPrefName(prefList, key) ?? key}
+                        key={key}
+                        stroke={generateColor(index, keys.length)}
+                      />
+                    ))}
+                  </LineChart>
+                </ResponsiveContainer>
+              </div>
+            </>
+          ) : (
+            <p>データを取得中です...</p>
+          )}
         </div>
       )}
     </section>

068d9a33f045c394c47ad81306e97e0046a56669
Utopi-a <bridgestone.amatsu0228@gmail.com>
2024-06-22 18:45:09 +0900

    feat: モックアップで表示していた県一覧を，取得した都道府県情報から動的に生成するようにした。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,25 @@ import { PopulationTrendChart } from "@/features/components/PopulationTrendChart
 import { PrefectureCheckBoxGrid } from "@/features/components/PrefectureCheckBoxes/PrefectureCheckBoxGrid";
 import { trpc } from "@/utils/trpc";
 import { Inter } from "next/font/google";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const hello = trpc.getPrefectureList.useQuery();
+  const { data } = trpc.getPrefectureList.useQuery();
 
-  console.log(hello.data);
+  const [selectedPrefectures, setSelectedPrefectures] = useState<string[]>([]);
+
+  console.log(data);
 
   return (
     <div className="wrapper">
       <Header />
-      <PrefectureCheckBoxGrid />
+      <PrefectureCheckBoxGrid
+        prefList={data}
+        prefectures={selectedPrefectures}
+        setPrefectures={setSelectedPrefectures}
+      />
       <PopulationTrendChart />
     </div>
   );
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,10 @@
 import z from "zod";
 
-export const PrefApiResponseSchema = z.object({
-  message: z.string(),
-  result: z.object({
+export const PrefApiResponseSchema = z.array(
+  z.object({
     prefCode: z.number(),
     prefName: z.string(),
   }),
-});
+);
+
+export type PrefApiResponse = z.infer<typeof PrefApiResponseSchema>;

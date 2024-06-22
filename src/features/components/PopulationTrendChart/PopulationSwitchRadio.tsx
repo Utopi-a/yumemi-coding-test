d161427532542480d721a36834c8553bb438b526
Utopi-a <bridgestone.amatsu0228@gmail.com>
2024-06-22 22:55:40 +0900

    feat: 選択した都道府県をリセットするボタンを追加した。また，レスポンシブ対応等レイアウトの調整を行った。

--- src/features/components/PopulationTrendChart/PopulationSwitchRadio.tsx
+++ src/features/components/PopulationTrendChart/PopulationSwitchRadio.tsx
@@ -1,7 +1,5 @@
-import { SetStateAction } from "react";
-
 type PopulationSwitchRadioProps = {
-  setSelectedPopulationLabel: React.Dispatch<SetStateAction<string>>;
+  setSelectedPopulationLabel: React.Dispatch<React.SetStateAction<string>>;
 };
 
 export const PopulationSwitchRadio = ({
@@ -9,38 +7,46 @@ export const PopulationSwitchRadio = ({
 }: PopulationSwitchRadioProps) => {
   return (
     <div className="populationSelector">
-      <input
-        type="radio"
-        id="populationType1"
-        name="population"
-        value="総人口"
-        onClick={(e) => setSelectedPopulationLabel(e.currentTarget.value)}
-      />
-      <label htmlFor="populationType1">総人口</label>
-      <input
-        type="radio"
-        id="populationType2"
-        name="population"
-        value="年少人口"
-        onClick={(e) => setSelectedPopulationLabel(e.currentTarget.value)}
-      />
-      <label htmlFor="populationType2">年少人口</label>
-      <input
-        type="radio"
-        id="populationType3"
-        name="population"
-        value="生産年齢人口"
-        onClick={(e) => setSelectedPopulationLabel(e.currentTarget.value)}
-      />
-      <label htmlFor="populationType3">生産年齢人口</label>
-      <input
-        type="radio"
-        id="populationType4"
-        name="population"
-        value="老年人口"
-        onClick={(e) => setSelectedPopulationLabel(e.currentTarget.value)}
-      />
-      <label htmlFor="populationType4">老年人口</label>
+      <div className="radioItem">
+        <input
+          type="radio"
+          id="populationType1"
+          name="population"
+          value="総人口"
+          onClick={(e) => setSelectedPopulationLabel(e.currentTarget.value)}
+        />
+        <label htmlFor="populationType1">総人口</label>
+      </div>
+      <div className="radioItem">
+        <input
+          type="radio"
+          id="populationType2"
+          name="population"
+          value="年少人口"
+          onClick={(e) => setSelectedPopulationLabel(e.currentTarget.value)}
+        />
+        <label htmlFor="populationType2">年少人口</label>
+      </div>
+      <div className="radioItem">
+        <input
+          type="radio"
+          id="populationType3"
+          name="population"
+          value="生産年齢人口"
+          onClick={(e) => setSelectedPopulationLabel(e.currentTarget.value)}
+        />
+        <label htmlFor="populationType3">生産年齢人口</label>
+      </div>
+      <div className="radioItem">
+        <input
+          type="radio"
+          id="populationType4"
+          name="population"
+          value="老年人口"
+          onClick={(e) => setSelectedPopulationLabel(e.currentTarget.value)}
+        />
+        <label htmlFor="populationType4">老年人口</label>
+      </div>
     </div>
   );
 };
--- src/features/components/PrefectureCheckBoxes/PrefectureCheckBox.tsx
+++ src/features/components/PrefectureCheckBoxes/PrefectureCheckBox.tsx
@@ -2,12 +2,24 @@ type PrefectureCheckBoxProps = {
   prefName: string;
   prefCode: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  checked: boolean;
 };
 
-export const PrefectureCheckBox = ({ prefName, prefCode, onChange }: PrefectureCheckBoxProps) => {
+export const PrefectureCheckBox = ({
+  prefName,
+  prefCode,
+  onChange,
+  checked,
+}: PrefectureCheckBoxProps) => {
   return (
     <label className="prefCheckbox">
-      <input type="checkbox" name="prefectures" value={prefCode} onChange={onChange} />
+      <input
+        type="checkbox"
+        name="prefectures"
+        value={prefCode}
+        onChange={onChange}
+        checked={checked}
+      />
       <p>{prefName}</p>
     </label>
   );
